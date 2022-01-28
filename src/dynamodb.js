09b1d9ecef6e2f23c7d8425bdb30da5f496534f3
gitrepo-dev <jaysean.developer@gmail.com>
2022-01-28 19:36:08 +0530

    try 05

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,9 +1,9 @@
 // config dynamodb
 const {DynamoDBClient} = require("@aws-sdk/client-dynamodb");
 const client = new DynamoDBClient({
-    accessKeyId: "aws_id",
-    secretAccessKeyId: "aws_key",
-    endpoint: "http://localhost:8000"
+    // accessKeyId: "aws_id",
+    // secretAccessKeyId: "aws_key",
+    // endpoint: "http://localhost:8000"
 });
 
 module.exports = client;
